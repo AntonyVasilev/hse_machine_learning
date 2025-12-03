2d5b8515094d0c06ea485e77b45baa93d3893620
Anton Vasilev <annivasilev@edu.hse.ru>
2025-12-03 21:20:30 +0300

    add streamlit project

---
+++ app.py
@@ -0,0 +1,216 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from sklearn.metrics import r2_score, mean_absolute_percentage_error
+from pathlib import Path
+import phik  # noqa: F401
+import traceback
+from functions import load_model, prepare_features, load_transforms, prepare_loaded_dataframe, load_additional_data
+
+st.set_page_config(page_title="Cars' selling price prediction", layout="wide")
+
+MODEL_DIR = Path(__file__).resolve().parent / "models"
+MODEL_PATH = MODEL_DIR / "ridge_model.pkl"
+
+DATA_DIR = Path(__file__).resolve().parent / "data"
+FEATURE_NAMES_PATH = DATA_DIR / "feature_names.pkl"
+
+target_col_name = 'selling_price'
+
+# TODO: Реализовать предсказание через заполнение формы с фичами (после реализации предыдущего TODO)
+
+# Загружаем модель
+try:
+    model, feature_names = load_model(model_path=MODEL_PATH, feacture_names_path=FEATURE_NAMES_PATH)
+    feature_names = list(feature_names)
+    feature_names_with_target = feature_names.copy()
+    feature_names_with_target.append(target_col_name)
+except Exception as e:
+    st.error(f"Ошибка загрузки модели: {e}")
+    st.stop()
+
+try:
+    min_max_scaler, one_hot_encoder, ordinal_encoder, polynominal_features = load_transforms(path=MODEL_DIR)
+    cols_with_na, na_fill_mapping, columns, final_columns, main_columns, values_for_form, features_base_dtypes = load_additional_data(path=DATA_DIR)
+except Exception as e:
+    st.error(f"Ошибка загрузки для подготовки данных: {e}")
+    st.stop()
+
+
+# Визуализация весов модели
+st.subheader("Визуализация весов модели")
+
+chkbx = st.checkbox(label='Добавить интерсепт')
+
+weights = model.coef_
+df_to_plot = pd.DataFrame(data=pd.concat([
+    pd.Series(feature_names), 
+    pd.Series(weights)
+    ], axis=1)
+)
+df_to_plot.columns = ['feature_name', 'weight']
+
+if chkbx:
+    intercept_df = pd.DataFrame([{'feature_name': 'intercept', 'weight': model.intercept_}])
+    df_to_plot = pd.concat([df_to_plot, intercept_df], ignore_index=True)
+
+
+st.line_chart(df_to_plot, x='feature_name' ,y='weight', x_label='Weight', y_label='Feature names')
+
+
+st.subheader("Получение прогноза стоимости")
+
+pred_type = st.radio(
+    'Выберите тип получения прогноза',
+    ['Загрузка csv-файла', 'Ввод данных в форму'],
+    captions=[
+        'Предсказание по всем объектам из файла + графики',
+        'Предсказание по одному объекту на данных из формы'
+    ]
+)
+
+if pred_type == 'Загрузка csv-файла':
+    # Загрузка CSV файла
+    uploaded_file = st.file_uploader("Загрузите CSV файл", type=["csv"])
+
+    if uploaded_file is None:
+        st.info("Загрузите CSV файл для начала работы")
+        st.stop()
+
+    # Загружаем данные
+    df = pd.read_csv(uploaded_file)
+
+    # Визуализации данных
+    st.subheader("Визуализации данных")
+
+    df_to_plot = df.copy()
+
+    plot_col1, plot_col2 = st.columns(2)
+
+    with plot_col1:
+    # pie chart признака brand
+        df_to_plot['brand'] = df_to_plot['name'].apply(lambda val: val.split()[0])
+        n_cars_by_drand = (
+            df_to_plot.groupby('brand').agg(n_cars=('name', 'count')).reset_index().sort_values('n_cars'))
+        fig1 = px.pie(
+            n_cars_by_drand,
+            values='n_cars',
+            names='brand',
+            title="Количество автомобилей по брендам"
+        )
+        st.plotly_chart(fig1, width='stretch')
+    with plot_col2:
+        # Гистаграмма распрелделения целевой переменной
+        fig2 = px.histogram(
+            df_to_plot, 
+            x=target_col_name, 
+            marginal="box", 
+            title='Гистаграмма распрелделения целевой переменной'
+        )
+        st.plotly_chart(fig2, width='stretch')
+
+    plot_col3, plot_col4 = st.columns(2)
+    
+    with plot_col3:
+        corr_matrix = df_to_plot.phik_matrix()
+        fig3 = px.imshow(
+            corr_matrix, 
+            text_auto=True, 
+            color_continuous_scale='RdBu', 
+            title='Phik-матрица корреляций признаков'
+        )
+        st.plotly_chart(fig3, width='stretch')
+    with plot_col4:
+        mean_prices_by_year = df_to_plot.groupby(['year', 'seats']).agg(mean_price=(target_col_name, 'mean')).reset_index()
+        mean_prices_by_year['seats'] = mean_prices_by_year['seats'].astype('int')
+        fig4 = px.bar(
+            mean_prices_by_year, 
+            x='year', 
+            y='mean_price', 
+            color='seats', 
+            color_continuous_scale='RdBu',
+            title='Средние цены в по годам с учетом количества сидений в автомобиле'
+        )
+        st.plotly_chart(fig4, width='stretch')
+
+    df_prepared = prepare_loaded_dataframe(
+        data=df,
+        cols_with_na=cols_with_na,
+        na_fill_mapping=na_fill_mapping,
+        feature_names=feature_names_with_target,
+        columns=columns,
+        final_columns=final_columns,
+        min_max_scaler=min_max_scaler,
+        one_hot_encoder=one_hot_encoder,
+        ordinal_encoder=ordinal_encoder,
+        polynominal_features=polynominal_features
+    )
+
+    try:
+        df_prepared = prepare_features(df_prepared, feature_names)
+        X, y = df_prepared, df[target_col_name]
+        prediction = model.predict(X)
+    except Exception as e:
+        st.error(f"Ошибка при обработке данных: {e}")
+        st.error(traceback.format_exc())
+        st.stop()
+        
+    st.subheader("Метрики качества предсказания")
+
+    # Метрики
+    metric_col1, metric_col2 = st.columns(2)
+    with metric_col1:
+        r2 = r2_score(y, prediction)
+        st.metric("R2", f"{r2:.2f}", help='Коэффицент детерминации R-квадрат')
+    with metric_col2:
+        rmse = mean_absolute_percentage_error(y, prediction) * 100
+        st.metric("MAPE", f"{rmse:.2f}%", help='Средняя абсолютная процентная ошибка')
+
+else:
+    st.subheader("Сделать предсказание для нового клиента")
+
+    with st.form("prediction_form"):
+        col_left, col_right = st.columns(2)
+        input_data = {}
+        
+        with col_left:
+            st.write("**Категориальные признаки:**")
+            for col in main_columns:
+                if features_base_dtypes[col] in ('object', 'bool'):
+                    # unique_vals = sorted(df[col].astype(str).unique().tolist())
+                    input_data[col] = st.selectbox(col, values_for_form[col], key=f"cat_{col}")
+        
+        with col_right:
+            st.write("**Числовые признаки:**")
+            for col in main_columns:
+                if features_base_dtypes[col] not in ('object', 'bool'):
+                    # val = df[col].median()                
+                    input_data[col] = st.number_input(col, value=values_for_form[col], key=f"num_{col}")
+
+        submitted = st.form_submit_button("Получить предсказание", width='stretch')
+
+    if submitted:
+        result = st.empty()
+        try:
+            input_df = pd.DataFrame([input_data])
+            input_df_prepared = prepare_loaded_dataframe(
+                data=input_df,
+                cols_with_na=cols_with_na,
+                na_fill_mapping=na_fill_mapping,
+                feature_names=feature_names_with_target,
+                columns=columns,
+                final_columns=final_columns,
+                min_max_scaler=min_max_scaler,
+                one_hot_encoder=one_hot_encoder,
+                ordinal_encoder=ordinal_encoder,
+                polynominal_features=polynominal_features
+            )
+            input_df_prepared = prepare_features(input_df_prepared, feature_names)
+            prediction = model.predict(input_df_prepared)
+            prediction = round(float(prediction[0]), 2)
+
+            result.success(f"**Предсказанная стоимость автомобиля:** {prediction}")
+        except Exception as e:
+            st.error(f"Ошибка при предсказании: {e}")
+            st.error(traceback.format_exc())
+
---
+++ functions.py
@@ -0,0 +1,247 @@
+import streamlit as st # type: ignore
+import pickle
+import json
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, PolynomialFeatures
+
+
+poly_cols = ['mileage', 'engine', 'max_power', 'torque', 'max_torque_rpm']
+ohe_cols = ['fuel', 'seller_type', 'transmission', 'owner', 'seats']
+
+
+def prepare_loaded_dataframe(
+    data: pd.DataFrame, 
+    cols_with_na: list, 
+    na_fill_mapping: dict,
+    feature_names: list,
+    columns: list,
+    final_columns: list,
+    min_max_scaler: MinMaxScaler, 
+    one_hot_encoder: OneHotEncoder, 
+    ordinal_encoder: OrdinalEncoder, 
+    polynominal_features: PolynomialFeatures
+    ) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        data (pd.DataFrame): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    df = data.copy()
+
+    # Приведение признаков 'mileage', 'engine', 'max_power' к формату float
+    for col_ in ['mileage', 'engine', 'max_power']:
+        df[col_] = df[col_].apply(convert_to_float)
+
+    # Обработка значений признака torque - парсинг числовых значений
+    cleaned_torque = df['torque'].apply(parce_torque)
+    cleaned_torque_df = pd.DataFrame(cleaned_torque.to_list(), columns=['torque', 'max_torque_rpm'])
+    df = pd.concat([df.drop('torque', axis=1), cleaned_torque_df], axis=1)
+
+    # Сохраняю маски полей, которые были с пропущенными значениями, по каждому из заполняемых признаков 
+    na_values_mask = {col: df[col].isna() for col in cols_with_na}
+
+    # Заполнение медианой пропущенных значений
+    df = df.fillna(na_fill_mapping)
+
+    # Привожу значения seatsк целочисленному типу
+    df['seats'] = df['seats'].astype('int')
+
+    # Проделываю аналогичные преобразования с тестовыми данными
+    df_prepared = df.select_dtypes(include='object')
+    df_prepared = pd.concat([df_prepared, df[columns]], axis=1)
+    df_prepared['name'] = df_prepared['name'].apply(lambda val: val.split()[0].strip())
+    df_prepared['name'] = ordinal_encoder.transform(df_prepared[['name']])
+    df_prepared['name'] = df_prepared['name'].astype('int')
+    df_prepared = pd.concat([
+        df_prepared,
+        pd.DataFrame(data=polynominal_features.transform(df_prepared[poly_cols]), 
+                     columns=polynominal_features.get_feature_names_out()).drop(poly_cols + ['1'], axis=1)
+    ], axis=1)
+    encoded_cols_test = pd.DataFrame(data=one_hot_encoder.fit_transform(df_prepared[ohe_cols]).toarray(), 
+                                     columns=one_hot_encoder.get_feature_names_out())
+    df_prepared = pd.concat([df_prepared.drop(ohe_cols, axis=1), encoded_cols_test], axis=1)
+
+    # Добавляю недостающие в тесте столбцы
+    cols_to_add = list(set(feature_names) - set(df_prepared.columns))
+    df_prepared[cols_to_add] = 0
+    df_prepared = df_prepared[final_columns]
+
+    # Маштабирую данные с помощью MinMaxScaler
+    df_prepared = pd.DataFrame(data=min_max_scaler.transform(df_prepared), columns=df_prepared.columns)
+
+    for col, mask in na_values_mask.items():
+        df_prepared[f"is_nan_val_{col}"] = mask.astype('int')
+    
+    return df_prepared
+
+
+@st.cache_resource
+def load_transforms(path: Path) -> tuple:
+    # min_max_scaler
+    with open(path / 'min_max_scaler.pkl', 'rb') as f:
+        min_max_scaler = pickle.load(f)
+
+    # one_hot_encoder
+    with open(path / 'one_hot_encoder.pkl', 'rb') as f: 
+        one_hot_encoder = pickle.load(f)
+
+    # ordinal_encoder
+    with open(path / 'ordinal_encoder.pkl', 'rb') as f:
+        ordinal_encoder = pickle.load(f)
+
+    # polynominal_features
+    with open(path / 'polynominal_features.pkl', 'rb') as f:
+        polynominal_features = pickle.load(f)
+
+    return min_max_scaler, one_hot_encoder, ordinal_encoder, polynominal_features
+
+
+@st.cache_resource
+def load_additional_data(path: Path) -> tuple:
+    # cols_with_na
+    with open(path / 'cols_with_na.json', 'rb') as f:
+        cols_with_na = json.load(f)
+
+    # columns
+    with open(path / 'columns.json', 'rb') as f:
+        columns = json.load(f)
+
+    # features_base_dtypes
+    with open(path / 'features_base_dtypes.pkl', 'rb') as f:
+        features_base_dtypes = pickle.load(f)
+
+    # final_columns
+    with open(path / 'final_columns.json', 'rb') as f:
+        final_columns = json.load(f)
+
+    # main_columns
+    with open(path / 'main_columns.json', 'rb') as f:
+        main_columns = json.load(f)
+    
+    # na_fill_mapping
+    with open(path / 'na_fill_mapping.json', 'rb') as f:
+        na_fill_mapping = json.load(f)
+    
+    # values_for_form
+    with open(path / 'values_for_form.json', 'rb') as f:
+        values_for_form = json.load(f)
+    
+    return cols_with_na, na_fill_mapping, columns, final_columns, main_columns, values_for_form, features_base_dtypes
+
+
+@st.cache_resource
+def load_model(model_path, feacture_names_path):
+    """Загружаем модель через pickle"""
+
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+    with open(feacture_names_path, 'rb') as f:
+        feature_names = pickle.load(f)
+    return model, feature_names
+
+
+def prepare_features(df, feature_names):
+    """Приводим данные к формату обучения модели."""
+    df_proc = df.copy()
+    # Преобразуем категориальные признаки в строки (как при обучении)
+    for col in feature_names:
+        if col in df_proc.columns:
+            if df_proc[col].dtype in ('object', 'bool'):
+                df_proc[col] = df_proc[col].astype(str)
+    return df_proc[feature_names]
+
+
+def convert_to_float(val):
+    if val is not np.nan:
+        splitted_val = val.split()
+        val_to_convert = splitted_val[0] if splitted_val else '0'
+        if not val_to_convert.isalpha():
+            return float(val_to_convert)
+        else:
+            return 0.0
+
+
+def convert_to_float_torque(val):
+  if ',' in val:
+    val = ''.join(val.split(','))
+  elif '.' in val:
+    val = ''.join(val.split('.'))
+  return float(val)
+
+
+def parce_2_part(value):
+  if '+' in value:
+    splitted_values = value.strip().split('+')
+    splitted_val_1 = convert_to_float_torque(splitted_values[0])
+    splitted_val_2 = convert_to_float_torque(splitted_values[1].split('rpm')[0].strip().split('-')[1].strip())
+    max_torque_rpm = splitted_val_1 + splitted_val_2
+  elif '-' in value:
+      max_torque_rpm = max([convert_to_float_torque(val.strip())
+        for val in value.lower().split('rpm')[0].split('-')
+        if convert_to_float_torque(val.strip())])
+  elif '~' in value:
+    max_torque_rpm = max([convert_to_float_torque(val.strip())
+      for val in value.lower().split('rpm')[0].split('~')
+      if convert_to_float_torque(val.strip())])
+  else:
+    max_torque_rpm = convert_to_float_torque(
+        value.lower().split('rpm')[0].strip())
+  return max_torque_rpm
+
+
+def parce_torque(value):
+  if (value is None) | (value is np.nan) | (value == ''):
+    return None, None
+
+  try:
+    # Обрабатываю разные варианты разделителей
+    if '@' in value:
+      split_sign = '@'
+    elif 'at' in value:
+      split_sign = 'at'
+    elif '/' in value:
+      split_sign = '/'
+    else:
+      if 'nm' in value.lower():
+        return convert_to_float_torque(value.lower().split('nm')[0].strip()), None
+      else:
+        raise ValueError(f'Unknoun split sign: {value}')
+
+    splitted_value = value.split(split_sign)
+    # Обрабатываю 1 часть строки:
+    value_1 = splitted_value[0]
+    if 'nm' in value_1.lower():
+      torque = convert_to_float_torque(value_1.lower().split('nm')[0].strip())
+    elif 'kgm' in value_1.lower():
+      torque = convert_to_float_torque(
+          value_1.lower().split('kgm')[0].strip()) * 9.80665
+    elif '(' in value_1:
+      torque = convert_to_float_torque(value_1.split('(')[0].strip())
+    else:
+      torque = convert_to_float_torque(value_1.strip()) * 9.80665
+
+    # Обрабатываю 2 часть строки:
+    value_2 = splitted_value[1:]
+    if len(value_2) > 1:
+      value_2 = value_2[0].lower().split('(')[0].strip()
+      max_torque_rpm = parce_2_part(value_2)
+    else:
+      value_2 = value_2[0]
+      max_torque_rpm = parce_2_part(value_2)
+  except Exception as e:
+    print(e)
+    print(value)
+  else:
+    return torque, max_torque_rpm
+
+
+
+
+
+
